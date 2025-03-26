2ba85fd04cd6b627ca57ba9f5b7cdab0e4e1fb4d
jhynjngf <raditezahra750@gmail.com>
2025-03-26 21:52:35 +0700

    Add files via upload

---
+++ Latihan 7.2.php
@@ -0,0 +1,7 @@
+<?php
+    $nilai=90;
+    if ($nilai>=90) {
+    echo "Nilai Anda : ". $nilai;
+    echo "<br>";
+    echo "Nilai mata kuliah pengenalan web anda A";}
+?>
---
+++ Latihan 7.3.php
@@ -0,0 +1,7 @@
+<?php
+$nama="Mahasiswa";
+if ($nama=="Mahasiswa")
+    echo "Selamat Datang di Politeknik Negeri Subang";
+else
+    echo "Anda tidak terdaftar sebagai mahasiswa"
+?>
